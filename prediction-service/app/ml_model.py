6d912631afc7011e1e2f4808544e6773c75af159
Сергей Вершинин <Sergey@integro.local>
2024-01-16 00:34:50 +0500

    add ml predictor to prediction-service

--- prediction-service/app/ml_model.py
+++
@@ -1,7 +0,0 @@
-from app.dto_models import AtmData
-
-
-class Model:
-
-    def predict(self, atm_data_list: list[AtmData]):
-        return [0. for _ in atm_data_list]
---
+++ prediction-service/app/predictor/ml_model.py
@@ -0,0 +1,83 @@
+import pickle
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import StackingRegressor
+from catboost import CatBoostRegressor
+
+import numpy as np
+
+from app.dto_models import AtmData
+import pandas as pd
+
+REQUIRED_FEATURE_LIST = ['atm_group', 'lat', 'long', 'city', 'federal_district', 'region_with_type', 'locality_area',
+                         'locality_population', 'sustenance', 'education', 'fuel', 'car_service', 'parking_space',
+                         'atm',
+                         'bank', 'bureau_de_change', 'outpatient_medical_facilities', 'inplace_medical_facilities',
+                         'pharmacy', 'veterinary', 'entertainment', 'entertainment_for_adults',
+                         'administrative_buildings', 'police', 'fire_station', 'post_office', 'grave_yard',
+                         'marketplace', 'monastery', 'place_of_worship', 'public_transport_stop_position',
+                         'alcohol_shop', 'food_shop', 'supermarket', 'mall', 'wholesale', 'clothing_shop',
+                         'discount_store', 'beauty_store', 'hardware_store', 'interior_store', 'electronics_store',
+                         'sport_store', 'auto_moto_store', 'car_parts_store', 'hobbies_store', 'books_store', 'hotel',
+                         'museum']
+
+
+class Predictor:
+
+    def __init__(self):
+        self._load_components()
+
+    def _load_components(self):
+        with open("app/predictor/Imputer.pickle", "rb") as f:
+            self.imputer: SimpleImputer = pickle.load(f)
+        with open("app/predictor/Encoder.pickle", "rb") as f:
+            self.encoder: OneHotEncoder = pickle.load(f)
+        with open("app/predictor/Scaler.pickle", "rb") as f:
+            self.scaler: StandardScaler = pickle.load(f)
+        with open("app/predictor/Model.pickle", "rb") as f:
+            self.model: StackingRegressor = pickle.load(f)
+
+    def predict(self, atm_data_list: list[AtmData]) -> list[float]:
+        df = pd.DataFrame(dict(item) for item in atm_data_list)
+        df = self.enrich_df(df)
+        self.assert_feature_presence_and_order(df)
+
+        df = self.preprocess_data(df)
+        return self.model.predict(df).tolist()
+
+    def enrich_df(self, df):
+        # меняем lon на long, т.к. в датасете оно long
+        df.rename(columns={"lon": "long"}, inplace=True)
+
+        # создаем недостающие столбцы и заполняем их NaN, чтобы потом их заполнил Imputer
+        for col in REQUIRED_FEATURE_LIST:
+            if col not in df.columns:
+                df[col] = np.NaN
+
+        # возвращаем датафрейм, но с тем порядком столбцов, который задан в требованиях
+        return df[REQUIRED_FEATURE_LIST]
+
+    def assert_feature_presence_and_order(self, df: pd.DataFrame) -> None:
+        assert df.columns.tolist() == REQUIRED_FEATURE_LIST
+
+    def preprocess_data(self, df):
+        # заполняем пропуски
+        df[df.columns] = self.imputer.transform(df)
+
+        # кодируем категориальные признаки
+        cat_cols = ['atm_group', 'federal_district', 'city', 'region_with_type']
+        encoded_columns_df = pd.DataFrame(self.encoder.transform(df[cat_cols]).toarray(),
+                                          columns=self.encoder.get_feature_names_out(cat_cols),
+                                          dtype=int)
+        df = pd.concat(
+            [
+                df.drop(columns=cat_cols).reset_index(drop=True),
+                encoded_columns_df.reset_index(drop=True)
+            ],
+            axis=1
+        )
+
+        # масштабируем признаки
+        df = pd.DataFrame(self.scaler.transform(df), columns=df.columns)
+        return df
